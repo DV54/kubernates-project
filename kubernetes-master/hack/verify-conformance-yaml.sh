71859a8986d3290ad6c72936199bde39db734314
Digvijay Singh <digvijaysingh54856@gmail.com>
2024-01-04 02:01:16 +0530

    first commit

---
+++ kubernetes-master/hack/verify-conformance-yaml.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# Copyright 2021 The Kubernetes Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+KUBE_ROOT=$(dirname "${BASH_SOURCE[0]}")/..
+cd "${KUBE_ROOT}"
+
+# generate json spec -> yaml
+test/conformance/gen-conformance-yaml.sh
+
+# diff generated and checked-in
+if ! diff -u test/conformance/testdata/conformance.yaml _output/conformance.yaml; then
+  echo FAIL
+  echo 'See instructions in test/conformance/README.md'
+  exit 1
+fi
+
+# generate image list
+test/conformance/gen-test-image-list.sh
+
+# diff generated and checked-in
+if ! diff -u test/conformance/testdata/repo_list_images.txt _output/repo_list_images.txt; then
+  echo FAIL
+  exit 1
+fi
+exit 0
